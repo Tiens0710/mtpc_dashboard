bd35c4e2a4ee178e1352927cf2a81ab500113d1a
Tiens0710 <tengiadd7@gmail.com>
2026-09-06 17:37:48 +0700

    Fix mobile course index toggle positioning

--- moodle-theme/mtpc/version.php
+++ moodle-theme/mtpc/version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_mtpc';
-$plugin->version = 2026090609;
+$plugin->version = 2026090610;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
